6810f51ea9529e211fed8f1385e884315d162a1f
Matthias-Sin <Matthias.sin26@ycdsbk12.ca>
2025-05-23 09:39:59 -0400

    add some testing code

--- CPTMatthiasS.java
+++ CPTMatthiasS.java
@@ -3,6 +3,19 @@ import arc.*;
 public class CPTMatthiasS{ 
 	public static void main(String[] args){ 
 		Console con = new Console();
+		String strgood;
+		String stridk; 
+		String strworse;
+		String strrude; 
+		String strAnswer;
 		con.println("hello world"); 
+		con.println("How are you today?"); 
+		con.println("A. I'm good");
+		con.println("B. IDK"); 
+		con.println("C. I'm worse"); 
+		con.println("D. I HATE THE WORLD"); 
+		strAnswer = con.readLine(); 
+	
+		
 	}
 }
