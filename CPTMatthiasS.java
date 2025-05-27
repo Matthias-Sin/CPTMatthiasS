e79e19ea6c5b08ab5d27a9d15a08ab7e37030042
Matthias-Sin <Matthias.sin26@ycdsbk12.ca>
2025-05-27 12:27:07 -0400

    Add textinputfile

--- CPTMatthiasS.java
+++ CPTMatthiasS.java
@@ -4,6 +4,7 @@ public class CPTMatthiasS{
 	public static void main(String[] args){ 
 		Console con = new Console();
 		con.println("Hello world"); 
-		
+		TextInputFile Countries = new TextInputFile("Countries.txt"); 
+		TextInputFile MCU = new TextInputFile("MCU.txt"); 
 	}
 }
