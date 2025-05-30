225dcc5d8dc7a9822ad2ce847a1d10c7af2f4353
Matthias-Sin <Matthias.sin26@ycdsbk12.ca>
2025-05-30 08:39:22 -0400

    Added Menu Background, added some questions

--- CPTMatthiasS.java
+++ CPTMatthiasS.java
@@ -1,10 +1,29 @@
 import arc.*; 
+import java.awt.Color;
+import java.awt.image.BufferedImage; 
+import java.awt.Font; 
 
 public class CPTMatthiasS{ 
 	public static void main(String[] args){ 
-		Console con = new Console();
-		con.println("Hello world"); 
-		TextInputFile Countries = new TextInputFile("Countries.txt"); 
-		TextInputFile MCU = new TextInputFile("MCU.txt"); 
+		Console con = new Console(1280,720);
+		
+		int intX; 
+		int intY; 
+		
+		
+		
+		while(true){ 
+			//set main menu background color
+			con.setDrawColor(new Color(128, 128, 128)); 
+			con.fillRect(0,0,1280,720); 
+			
+			BufferedImage logo = con.loadImage("Mc.jpg"); 
+			con.drawImage(logo, 540, 200); 
+			
+		}
+
+		
+		
+			
 	}
 }
