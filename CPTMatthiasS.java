e4453139167c0e6d6f5e5ffc902f920a62497cbf
Matthias-Sin <Matthias.sin26@ycdsbk12.ca>
2025-06-02 08:49:07 -0400

    Add method file, add textinputfile

--- CPTMatthiasS.java
+++ CPTMatthiasS.java
@@ -7,17 +7,24 @@ public class CPTMatthiasS{
 	public static void main(String[] args){ 
 		Console con = new Console(1280,720);
 		
+		//Create variables
 		int intX; 
 		int intY; 
 		
-		
+		//load game textfile
+		TextInputFile MCU = new TextInputFile("MCU.txt"); 
+		//Math Questions = MQ
+		TextInputFile MQ = new TextInputFile("Math.txt"); 
+		TextInputFile Countries = new TextInputFile("Countries.txt"); 
+		//The secret game
+		TextInputFile secret = new TextInputFile("ItalianBrainrot.txt"); 
 		
 		while(true){ 
 			//set main menu background color
 			con.setDrawColor(new Color(80, 0, 40)); 
 			con.fillRect(0,0,1280,720); 
 			
-			BufferedImage logo = con.loadImage("Mc.jpg"); 
+			BufferedImage logo = con.loadImage(""); 
 			con.drawImage(logo, 0, 0); 
 			
 		}
